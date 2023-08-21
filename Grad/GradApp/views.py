616c97dbdd1ca3eaeed56ae0e5dee51108b7ed93
Mervez-Flame <mervez2006@gmail@gmail.com>
2023-08-21 20:57:04 +0100

    fifth commit

--- Grad/GradApp/views.py
+++ Grad/GradApp/views.py
@@ -10,54 +10,57 @@ def home (request):
     return render(request, 'home.html')
 
 def loginPage (request):
-    return render(request, 'loginPage.html')
+    return render(request, 'login_view.html')
 
 def contact(request):
     return render(request, 'contact.html')
 
 def logout (request):
     auth.logout(request)
-    return render(request, 'home')
+    return redirect('home')
 
-def login (request):
+def login_view(request):
     if request.method == 'POST':
-        username = request.POST['uname']
-        password = request.POST['psw']
+        username = request.POST.get('uname')  # Use get() to safely get the POST data
+        password = request.POST.get('psw')
+        
         user = auth.authenticate(username=username, password=password)
-    if user is not None:
-        auth.login(request, user)
-        messages.success(request, "Logged In Succesfully")
-        return redirect('home')
-    else:
-        messages.error(request, "Wrong username or password")
-        return redirect('loginPage')
+        
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "Logged In Successfully")
+            return redirect('home')
+        else:
+            messages.error(request, "Wrong username or password")
+            return redirect('loginPage')  # Make sure 'loginPage' is a valid URL name
     return render(request, 'home.html')
 
-def signup(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password1')
-        confirmPassword = request.POST.get('password2')
 
-        if password == confirmPassword:
-            if User.objects.filter(username=username).exists():
-                messages.info(request, 'Username already used')
-                return redirect('signup')
-            else:
-                user = User.objects.create_user(
-                    username=username, password=password)
-                user.save()
-                messages.success(request, 'Signup Successfully')
-                return redirect('loginPage')
-        else:
-            messages.error(request, 'Password not the same')
-            return redirect('signUpPage')
-    else:
-        return render(request, 'signUpPage.html')
+# def signup(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         email = request.POST.get('email')
+#         password = request.POST.get('password1')
+#         confirmPassword = request.POST.get('password2')
+
+#         if password == confirmPassword:
+#             if User.objects.filter(username=username).exists():
+#                 messages.info(request, 'Username already used')
+#                 return redirect('signup')
+#             else:
+#                 user = User.objects.create_user(
+#                     username=username, password=password)
+#                 user.save()
+#                 messages.success(request, 'Signup Successfully')
+#                 return redirect('loginPage')
+#         else:
+#             messages.error(request, 'Password not the same')
+#             return redirect('signUpPage')
+#     else:
+#         return render(request, 'signUpPage.html')
     
-def signUpPage(request):
-    return render(request, 'signUpPage.html')
+# def signUpPage(request):
+#     return render(request, 'register.html')
 
 def register(request):
     if request.method == 'POST':
@@ -70,18 +73,18 @@ def register(request):
             if User.objects.filter(email=email).exists():
                 messages.info(request, 'Email Already Used')
                 return redirect('register')
-        elif User.object.filter(username=username).exists():
-            messages.info(request, 'Username Already Exists')
-            return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already Exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password1)
+                user.save();
+                return redirect('home')
         else:
-            user = User.objects.create_user(username=username, email=email, password=password1)
-            user.save();
-            return redirect(login)
+            messages.info(request, 'Password Not The Same')
+            return redirect('register')
     else:
-        messages.info(request, 'Password Not The Same')
-        return redirect('register')
-        
-    return render(request, 'register.html')
+        return render(request , 'register.html') 
 
 def booking(request):
     return render(request, 'Booking.html')
